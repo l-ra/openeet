4a4c3989acf4e46d532c8892be45df6e987ebc14
lra <lra@r73.info>
2016-08-16 21:17:26 +0200

    shell converted to v3 eet api

--- shell/send-req.sh
+++ shell/send-req.sh
@@ -4,4 +4,4 @@ curl -XPOST \
    -H "Content-Type: text/xml;charset=UTF-8" \
    -H "SOAPAction: http://fs.mfcr.cz/eet/OdeslaniTrzby" \
    --data-binary @$1\
-   https://pg.eet.cz/eet/services/EETServiceSOAP/v2
+   https://pg.eet.cz/eet/services/EETServiceSOAP/v3
