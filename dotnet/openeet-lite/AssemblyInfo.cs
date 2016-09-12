66a9ecf83dffcf125fbb47e3c4828962f355659f
vlastikcocek <vlastik.c@seznam.cz>
2016-09-12 14:12:04 +0200

    Add files via upload

---
+++ dotnet/openeet-lite/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+#region Using directives
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle ("OpenEET")]
+[assembly: AssemblyDescription ("Komunikace s EET")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("OpenEET")]
+[assembly: AssemblyCopyright ("")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible (false)]
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all the values or you can use the default the Revision and 
+// Build Numbers by using the '*' as shown below:
+[assembly: AssemblyVersion ("1.0.3")]
+[assembly: Guid ("94e9e208-13f1-47da-a6c6-7c24c6af3420")]
